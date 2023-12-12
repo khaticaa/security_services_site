eadb51d94ab2c9c63fe2a4889e593cc783ef638a
khaticaa <mirzoevaxedice0@gmail.com>
2023-12-12 16:12:44 +0400

    sinif

--- js/details.js
+++ js/details.js
@@ -12,29 +12,21 @@ window.addEventListener("scroll",()=>{
         nav.style.transition = "all 1s ease";
     }
 })
-
-
-
-
-let s1bottom = document.querySelector(".s1bottom")
- function Service(){
+let detal = document.querySelector('.detal')
+function Service(){
     fetch('http://localhost:3000/services')
     .then(res => res.json())
     .then(data =>{
-        // s1bottom.innerHTML = "";
+    
         data.forEach(element => {
-            s1bottom.innerHTML += `
+        detal.innerHTML += `
             <div class="s1box">
             <div class="img">
                 <img src="${element.Image}" alt="">
             </div>
             <h3>${element.name}</h3>
             <p>${element.contanet}</p>
-            <i class="bi bi-heart"></i>
-            <div class="buttons">
-                <button>Delete</button>
-                <button>Update</button>
-            </div>
+            
         </div>
             `
         })
